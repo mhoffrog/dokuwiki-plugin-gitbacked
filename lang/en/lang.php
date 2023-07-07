0a34a7e9e3354fc7c24beee36748332528509f40
Markus Hoffrogge <mhoffrogge@gmail.com>
2023-07-07 07:30:51 +0200

    Add Gitbacked Admin UI -(work in progress)
    - Add initial work in progress
    
    - lang.php:
      - add texts for Admin

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -4,3 +4,4 @@ $lang['mail_create_new_error_subject'] = 'gitbacked plugin: The creation of a ne
 $lang['mail_repo_path_error_subject'] = 'gitbacked plugin: A FAILURE with the Git repo path occurred';
 $lang['mail_command_error_subject'] = 'gitbacked plugin: A Git command FAILED';
 $lang['mail_command_success_subject'] = 'gitbacked plugin: A Git command was performed';
+$lang['menu'] = 'Initialize Git repositories';
